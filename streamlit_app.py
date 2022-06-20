d267c45b5ae9749301e331b4e653c7e660b8bce3
CT <53102185+Aearsears@users.noreply.github.com>
2022-06-20 11:17:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,8 +78,8 @@ try:
     nlp = pipeline("multitask-qa-qg")
     faqs = nlp(text2)
     st.json(faqs)
+    st.code(json.dumps(faqs), language='python')
     url = 'https://cardify-backend.herokuapp.com/cards/qareceive'
-    print(json.dumps(faqs))
     x = requests.post(url, json=json.dumps(faqs))
     st.title("Status Code")
 
